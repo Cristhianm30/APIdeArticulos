c1edba30b6e1cacf444657616350e5915aa01f15
Cristhian Moreno <Cristhian.452@outlook.com>
2024-12-05 23:33:15 -0500

    Primer commit

---
+++ src/main/java/com/cristhian/apiarticulos/ApiDeArticulosApplication.java
@@ -0,0 +1,13 @@
+package com.cristhian.apiarticulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeArticulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeArticulosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristhian/apiarticulos/ApiDeArticulosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhian.apiarticulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeArticulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
